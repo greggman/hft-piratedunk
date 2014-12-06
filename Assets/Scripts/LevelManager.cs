4d59bfe698ba4524688d3d4f4d5f56ec61171728
Gregg Tavares <github@greggman.com>
2014-12-05 18:38:04 -0800

    initial commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using HappyFunTimes;
+
+public class LevelManager : MonoBehaviour {
+
+    public int numLocalPlayers = 0;
+
+    private LocalNetPlayer[] m_localPlayers;
+
+    void Start () {
+        numLocalPlayers = System.Math.Max(System.Math.Min(numLocalPlayers, 2000), 0);
+        m_localPlayers = new LocalNetPlayer[numLocalPlayers];
+
+        PlayerSpawner spawner = GetComponent<PlayerSpawner>();
+        for (int ii = 0; ii < numLocalPlayers; ++ii) {
+            LocalNetPlayer localPlayer = new LocalNetPlayer(spawner.GameServer);
+            m_localPlayers[ii] = localPlayer; //
+            spawner.StartLocalPlayer(localPlayer);
+        }
+    }
+}
